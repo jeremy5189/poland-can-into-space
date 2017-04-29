5999dd0c4ac80623c087b5b0c355f2d8db14dd4d
Jeremy Yen <jeremy5189@gmail.com>
2017-04-29 14:40:13 +0800

    print sat pos

--- trabant.js
+++ trabant.js
@@ -5,19 +5,7 @@ var tleLine1 = '1 25544U 98067A   13149.87225694  .00009369  00000-0  16828-3 0 
 // Initialize a satellite record
 var satrec = satellite.twoline2satrec(tleLine1, tleLine2);
 
-//  Or you can use a JavaScript Date
-var positionAndVelocity = satellite.spg4(satrec, new Date());
-
-// The position_velocity result is a key-value pair of ECI coordinates.
-// These are the base results from which all other coordinates are derived.
-var positionEci = positionAndVelocity.position,
-    velocityEci = positionAndVelocity.velocity;
-
-console.log(positionEci, velocityEci);
-
-// Set the Observer at 122.03 West by 36.96 North, in RADIANS
-var observerGd = {
-    longitude: -122.0308 * deg2rad,
-    latitude: 36.9613422 * deg2rad,
-    height: 0.370
-};
+for( var t = 0; t < 87; t++ ) {
+	var positionAndVelocity = satellite.sgp4(satrec, t);
+	console.log(positionAndVelocity.position);
+}
