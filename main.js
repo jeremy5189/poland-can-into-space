133ccaa78252e59c724b0c8ebe206750afa95b99
Scott Chen <one5809@gmail.com>
2017-04-29 17:25:30 +0800

    drag earth

--- main.js
+++ main.js
@@ -12,7 +12,8 @@ var camera	= new THREE.PerspectiveCamera(
 	45, window.innerWidth / window.innerHeight, 0.01, 100 
 );
 
-camera.position.z = 3; // 越小畫面越大
+var radius = 3
+camera.position.z = radius;
 
 // 增加整體光
 var light	= new THREE.AmbientLight(0x999999, 2)
@@ -23,18 +24,17 @@ var earthMesh	= THREEx.Planets.createEarth()
 scene.add(earthMesh)
 
 // 讓地球轉 
-updateFcts.push(function(delta, now){
+updateFcts[0]=function(delta, now){
 	earthMesh.rotation.y += 1/8 * delta;		
 	earthMesh.rotation.x += 1/16 * delta;	
-})
+}
 
 
 //////////////////////////////////////////////////////////////////////////////////
 //		Camera Controls							//
 //////////////////////////////////////////////////////////////////////////////////
-// var mouse	= {x : 0, y : 0}
+var mouse	= {x : 0, y : 0, oldX : 0, oldY : 0}
 document.addEventListener('wheel', function(event){
-	console.log(event.wheelDelta)
 	//scroll down
 	if(event.wheelDelta<0){
 		camera.position.z += 0.2
@@ -45,12 +45,58 @@ document.addEventListener('wheel', function(event){
 	}
 })
 
+var onMouseDownPhi=0,onMouseDownTheta=0;
+var theta,phi;
+document.addEventListener('mousedown', function(event){
+	updateFcts[0]=function(delta, now){
+		earthMesh.rotation.y = earthMesh.rotation.y;		
+		earthMesh.rotation.x = earthMesh.rotation.x;
+	}
+	
+	document.onmousemove = function(eveny){
+		mouse.x	= (event.screenX / window.outerWidth )
+		mouse.y	= (event.screenY / window.outerHeight)
+		theta = ( ( mouse.x - mouse.oldX ) * 3 ) + onMouseDownTheta;
+        phi = -( ( mouse.y - mouse.oldY ) * 3 ) + onMouseDownPhi;
+		
+		// console.log(phi)
+
+		if(phi >= Math.PI/2){
+			console.log(phi)
+			phi-=(Math.PI)
+		}
+		else if(phi <= -(Math.PI/2)){
+			phi+=Math.PI
+		}
+
+        camera.position.x = radius * Math.sin( theta) * Math.cos( phi );
+        camera.position.y = radius * Math.sin( phi );
+        camera.position.z = radius * Math.cos( theta) * Math.cos( phi );
+
+		camera.lookAt(scene.position)
+	}
+})
+document.addEventListener('mouseup', function(event){
+	updateFcts[0]=function(delta, now){
+		earthMesh.rotation.y += 1/8 * delta;		
+		earthMesh.rotation.x += 1/16 * delta;	
+	}
+	updateFcts[2]=function(delta,now){}
+	document.onmousemove = null;
+	onMouseDownPhi=phi;
+	onMouseDownTheta=theta;
+})
+document.addEventListener('mousemove', function(event){
+	mouse.oldX = (event.screenX / window.outerWidth )
+	mouse.oldY = (event.screenY / window.outerHeight)
+}, false)
+
 //////////////////////////////
 //		render the scene
 /////////////////////////////
-updateFcts.push(function(){
+updateFcts[1]=function(){
 	renderer.render( scene, camera );		
-})
+}
 
 //////////////////////////////
 //		looper runner
